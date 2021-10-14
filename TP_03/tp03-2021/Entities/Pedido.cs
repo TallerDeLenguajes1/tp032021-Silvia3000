42a5745b1bd553fbd321aa1682c9fc5860b401d1
silvia <programando2019@gmail.com>
2021-10-14 08:54:12 -0300

    falta un arreglo, pero subiendo

---
+++ TP_03/tp03-2021/Entities/Pedido.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tp03_2021.Entities
+{
+    public enum Estado { Recibido = 0, Realizandose, Entregado, Desconocido};
+    public class Pedido
+    {   
+        public int Id { get; set; }
+        public Cliente Cliente { get; set; }
+        public string Observaciones { get; set; }
+        public Estado Estado { get; set; }
+
+        public Pedido()
+        {
+            
+        }
+
+        public Pedido(int id, Cliente cliente, string observaciones, Estado estado)
+        {
+            Id = id;
+            Cliente = cliente;
+            Observaciones = observaciones;
+            this.Estado = estado;
+        }
+
+      
+    }
+}
